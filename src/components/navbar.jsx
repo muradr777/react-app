f68253069d7fa271b56aee941a3e2a48dfdc8da7
Murad <muradrustamov@gmail.com>
2019-01-01 05:57:14 +0400

    started project

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container">
+                <a class="navbar-brand" href="#">Navbar</a>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
